3589946a32a1ab4a093ef0de2b46e3886a4a55d6
Colin Campbell <999424+clncmbl@users.noreply.github.com>
2023-06-24 02:44:23 +0000

    Moved importmap to main.js.

--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,30 @@
 'use strict'
 
-// Intead of innerHTML trying document.currentScript together with
+// Can't use either innerHTML or insertAdjacentHTML to add a
+// script tag and expect it to execute.  Trying here to build
+// the desired script tag (for an importmap).
+// See https://github.com/WICG/import-maps#dynamic-import-map-example.
+
+const im = document.createElement('script')
+im.type = 'importmap'
+im.textContent = JSON.stringify({
+  "imports": {
+    "disableTextTable": "https://cdn.jsdelivr.net/gh/clncmbl/text-tags@main/docs/TextTable/TextTable.js",
+    "gh/": "https://cdn.jsdelivr.net/gh/clncmbl/text-tags@main/docs/",
+    "dev/": "/text-tags/"
+  }
+})
+document.currentScript.after(im)
+
+
+// Instead of innerHTML trying document.currentScript together with
 // insertAdjacentHTML.
 
+// Using :not(:defined) to hide undefined custom elements.  This
+// describes more options: 
+// https://www.abeautifulsite.net/posts/flash-of-undefined-custom-elements/
+
 document.currentScript.insertAdjacentHTML('afterend', `
-
-
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="main.css" blocking="render">
   <style>
@@ -17,5 +36,4 @@ document.currentScript.insertAdjacentHTML('afterend', `
       opacity: 0;
     }
   </style>
-
   `)
